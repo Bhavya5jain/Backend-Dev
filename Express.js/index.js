3a9c7dfa32e1ffb6100cb476e7d669a431ef336a
Bhavya Jain <jainbhavya0527@gmail.com>
2026-02-05 16:59:35 +0530

    CRUD OPERATIONS USING LOG FILES

--- Express.js/index.js
+++ Express.js/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require("fs");
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -10,22 +11,75 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-users=[
-    {id:1, name:"John",subjects:["Math","Science"]},
-    {id:2, name:"Jane",subjects:["English","History"]},
-    {id:3, name:"Doe",subjects:["Art","Physical Education"]}
-];
-app.get('/users', (req, res) => {
-    res.json(users);
-});
+// CRUD OPERATIONS
 
-
-app.get('/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find(u => u.id === userId);
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).send('User not found');
+app.get('/student/:id',(req,res)=>{
+    try {
+        const data = JSON.parse(fs.readFileSync("students.json","utf8"));
+        if(!data){
+            res.status(404).json({success:false,message:"Data not found"});
+        }
+        const id =Number(req.params.id);
+        const user=data.find(u => u.id === id);
+        console.log(user);
+        if(!user){
+            res.status(404).send("user not present")
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error.message);
     }
-});
+})
+
+app.post("/student/register",(req,res)=>{
+    try {
+        const newStudent=req.body;
+        const data = JSON.parse(fs.readFileSync("students.json","utf8"));
+        if(!data){
+            res.status(404).json({success:false,message:"Data not found"});
+        }
+        data.push(newStudent);
+        fs.writeFileSync("students.json",JSON.stringify(data));
+        res.status(200).json({success:true,message:"student successfully added"})
+    } catch (error) {
+        if(error){
+            res.status(500).json({success:false,message:error.message});
+        }
+    }
+})
+
+app.put("/student/:id",(req,res)=>{
+    try {
+    const id = req.params.id;
+    const newData=req.body;
+    const data = JSON.parse(fs.readFileSync("students.json","utf8"));
+    if(!data){
+        res.status(404).json({success:false,message:"Data not found"});
+    }
+    const idx = data.findIndex(u => u.id === id);
+    data[idx]={...data[idx],...newData};
+    fs.writeFileSync("students.json",JSON.stringify(data));
+    res.status(200).json({success:true,message:"student successfully updated"})
+    } catch (error) {
+        if(error){
+            res.status(500).json({success:false,message:error.message});
+        }
+    }
+})
+
+app.delete("/student/:id",(req,res)=>{
+    try {
+            const id = req.params.id;
+            const data = JSON.parse(fs.readFileSync("students.json","utf8"));
+             if(!data){
+                res.status(404).json({success:false,message:"Data not found"});
+            }
+            const idx = data.findIndex(u => u.id === id);
+            data.splice(idx,1);
+            res.status(200).json({success:true,message:"student successfully deleated"})
+    } catch (error) {
+        if(error){
+            res.status(500).json({success:false,message:error.message});
+        }
+    }
+})
